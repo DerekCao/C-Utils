7348fb67bfa07affd40d0f486550013899cebe64
DerekCao <wschy456@163.com>
2016-12-17 17:21:12 +0800

    create protocol utils

---
+++ Utils/GenericPro.c
@@ -0,0 +1,95 @@
+/*
+ * GenericPro.c
+ *
+ *  Created on: 2016年12月16日
+ *      Author: Administrator
+ *  一般协议捕捉,不进行拼包
+ *  协议中含有:包头+长度,则可以使用该函数
+ */
+#include "GenericPro.h"
+#include <memory.h>
+
+static int getFreeSlot(GENPRO *pro){
+	int i=0;
+	for(;i<pro->cath_max;i++){
+		if(!pro->state[i].use)
+			return i;
+	}
+	return CATCHER_ERROR;
+}
+
+static char isRecvHeaderOK(GENPRO *pro,int index){
+	int i=0,len;
+	for(;i<pro->headerSize;i++)
+		if(pro->buf[index][i]!=pro->header[i])
+			return 0;
+	len=pro->getLength(pro->buf[index],pro->state[index].cnt);
+	if(len>pro->maxSize||len<0)
+			return 0;
+	return 1;
+}
+
+static void add(GENPRO *pro,char *recv,int size){
+	int i=0,j,tmp;
+	for(;i<size;i++){
+		for(j=0;j<pro->cath_max;j++){
+			if(pro->state[j].use&&pro->state[j].cnt<pro->maxSize){	//填充缓冲区
+				pro->buf[j][pro->state[j].cnt]=recv[i];
+				pro->state[j].cnt++;
+			}
+		}
+		if(recv[i]==pro->header[0]){							//首字节相同,使能缓冲区
+			if((tmp=getFreeSlot(pro))!=CATCHER_ERROR){
+				pro->state[tmp].use=1;
+				pro->state[tmp].cnt=1;
+				pro->buf[tmp][0]=recv[i];
+				pro->state[i].bRecvHeader=0;
+			}
+		}
+	}
+}
+
+static void header(GENPRO *pro){
+	int i=0;
+	for(i=0;i<pro->cath_max;i++){
+		if(!pro->state[i].use)				//无效缓冲,不检测
+			continue;
+		if(pro->state[i].cnt<pro->minSize)	//过小,不检测
+			continue;
+		if(pro->state[i].bRecvHeader)		//已经检测 ,不检测
+			continue;
+		if(!isRecvHeaderOK(pro,i)){			//检测
+			pro->state[i].use=0;
+			continue;
+		}else{
+			pro->state[i].bRecvHeader=1;
+		}
+	}
+}
+
+static void complete(GENPRO *pro){
+	int i=0,length;
+	for(i=0;i<pro->cath_max;i++){
+		if(!pro->state[i].use)
+			continue;
+		if(!pro->state[i].bRecvHeader)
+			continue;
+		length=pro->getLength(pro->buf[i],pro->state[i].cnt);	//缓冲长度OK
+		if(pro->state[i].cnt<length)
+			continue;
+		if(pro->check(pro->buf[i],pro->state[i].cnt))			//校验OK,则回调
+			pro->callBack(pro->buf[i],length);
+		pro->state[i].use=0;									//缓冲无效
+	}
+}
+
+void Protocol_Init(GENPRO *pro){
+	memset(&pro->state,0,sizeof(pro->state));
+}
+
+void Protocol_AddBytes(GENPRO *pro,char *recv,int size){
+	add(pro,recv,size);
+	header(pro);
+	complete(pro);
+}
+
---
+++ Utils/GenericPro.h
@@ -0,0 +1,33 @@
+/*
+ * GenericPro.h
+ *
+ *  Created on: 2016年12月16日
+ *      Author: Administrator
+ */
+
+#ifndef GENERICPRO_H_
+#define GENERICPRO_H_
+
+#define CATCHER_MAX 10
+#define CATCHER_ERROR CATCHER_MAX+1
+
+typedef struct {
+	char use,bRecvHeader;
+	int cnt;
+}PROCAT;		//捕捉器,状态,初始化为0
+
+typedef struct {
+	const char *header;				//包头
+	const char headerSize,cath_max;	//包头大小,捕捉器非零缓存区个数
+	const int minSize,maxSize;		//信息最小长度,完整包最大长度
+	int (*getLength)(char *,int);	//获取长度信息
+	void (*callBack)(char *,int);	//回调函数
+	char (*check)(char *,int);		//校验
+	char *buf[CATCHER_MAX];			//捕捉器个数,减少丢包
+	PROCAT state[CATCHER_MAX];		//捕捉器状态
+}GENPRO;
+
+extern void Protocol_Init(GENPRO *);
+extern void Protocol_AddBytes(GENPRO *,char *,int); //字节接收
+
+#endif /* GENERICPRO_H_ */
